228036fc34829265a70b16bd6e367b71d027a11a
HaithemS <haithemsmirani@gmail.com>
2018-04-25 15:14:24 +0200

    2 new methods in Server class. one for sending and one for getting

--- src/main/java/iotsocketproject/Server/Server.java
+++ src/main/java/iotsocketproject/Server/Server.java
@@ -9,3 +9,23 @@ package iotsocketproject.Server;
  *
  * @author User
  */
+ public class Server{
+ 
+     
+     //Johns method!
+     public void sendTempToDatabase(String temp , String deviceId ){
+         //hårdkodade värden, ändra här.
+         temp = "30";
+         deviceId = "01";
+         dbHandler db = new dbHandler();
+         db.sendToDatabase(temp, deviceId);
+         
+     }
+     
+     //Adams method
+     public void GetFromDatabase(String temp, String deviceId){
+         dbHandler db = new dbHandler();
+         db.getFromDatabase();
+     }
+ 
+ }
