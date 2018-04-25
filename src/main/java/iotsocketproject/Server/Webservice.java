5821ea64b5f40f95529b53018604258c1e44ca13
BigAdam86 <31543595+BigAdam86@users.noreply.github.com>
2018-04-25 14:17:39 +0200

    added webservice

--- src/main/java/iotsocketproject/Server/Webservice.java
+++ src/main/java/iotsocketproject/Server/Webservice.java
@@ -1,14 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package iotsocketproject.Server;
 
-/**
- *
- * @author User
- */
-public class Webservice {
-    
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class Webservice extends HttpServlet{
+
+ private String value;
+ private String temp = "15";
+ private String[] historical;
+ 
+    public void init() throws ServletException 
+    {              
+           
+    }
+
+   public void doGet(HttpServletRequest request, 
+      HttpServletResponse response) throws ServletException, 
+      IOException{ 
+       value = request.getParameter("value");
+      response.setContentType("text/html");
+      PrintWriter out = response.getWriter();  
+      out.println("<!DOCTYPE html><HTML><HEAD><TITLE>Temperature Data</TITLE><meta charset=\"UTF-8\"></HEAD><BODY>");
+      out.println("<h1>Select what data you want to view:</h1>\n" +
+"       \n" +
+"        <form action = \"WebApp\" method = \"POST\">");
+      if ("1".equals(value)) out.println("<textarea readonly name=\"output\" rows=\"30\" cols=\"50\" placeholder=\"Your data will be displayed here...\">" +
+""+                                          "This is your current temp:"+temp+"</textarea>");
+      if ("2".equals(value)) out.println("<textarea readonly name=\"message\" rows=\"30\" cols=\"50\" placeholder=\"Your data will be displayed here...\">" +
+""+                                          "This is your historical temp</textarea>");
+      out.println("<br>\n" +
+"            <select name=\"value\">\n" +
+"                <option value=\"1\">Current</option>\n" +
+"                <option value=\"2\">Historical</option>\n" +
+"            </select>\n" +
+"            \n" +
+"            <input type = \"submit\" value = \"Submit\">\n" +
+"        </form>");
+      out.println("<a href=\"javascript:history.back()\">Go Back</a></BODY></HTML>");
+   } 
+   
+   public void doPost(HttpServletRequest request, 
+      HttpServletResponse response) throws ServletException, 
+      IOException{ 
+       doGet(request, response);
+   } 
 }
